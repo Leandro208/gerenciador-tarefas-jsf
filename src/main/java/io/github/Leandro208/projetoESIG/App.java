a3dbc39467a46539517bffac32bcaec791d36197
Leandro Barbosa <boyleandro_208@hotmail.com>
2022-12-31 02:23:51 -0300

    feat: adiciona estrutura inicial de projeto Maven

---
+++ src/main/java/io/github/Leandro208/projetoESIG/App.java
@@ -0,0 +1,13 @@
+package io.github.Leandro208.projetoESIG;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
